2065002d222d98182165bea342385ea7ac8086d5
Calbabreaker <calbabreaker@gmail.com>
2021-07-13 10:38:10 +0930

    Added basic uno game
    Now you can play uno by yourself

--- src/card/card.gd
+++ src/card/card.gd
@@ -1,14 +1,38 @@
-extends TextureRect
+extends TextureButton
+
+const card_colors := [
+	Color(0xd40b1fff), # red
+	Color(0x143dc9ff), # blue
+	Color(0xf2ce02ff), # yellow
+	Color(0x0fb82bff), # green
+]
 
 var color: Color
+var color_index: int
 var type: String
 
-func init(p_color: Color, p_type: String) -> Node:
-	color = p_color
+func init(p_type: String, p_color_index: int) -> Node:
+	color_index = p_color_index
 	type = p_type
+	set_color(card_colors[color_index])
 	var texture_specific = load("res://assets/cards/card_" + type + ".svg")
 	var texture_specific_modulate = load("res://assets/cards/card_" + type + "_modulate.svg")
 	$TextureSpecific.texture = texture_specific
 	$Front/TextureSpecificModulate.texture = texture_specific_modulate
-	$Front.modulate = color
 	return get_node(".")
+
+func set_color(p_color: Color):
+	color = p_color
+	$Front.modulate = color
+
+# these won't work with wildcards yet
+func is_lower_value(card: Node) -> bool:
+	var lower_type = int(type) < int(card.type)
+	var lower_color = color_index < card.color_index
+	return lower_color or (matches_color(card) and lower_type)
+	
+func matches_color(card: Node) -> bool:
+	return color_index == card.color_index
+
+func matches_number(card: Node) -> bool:
+	return type == card.type
--- src/game.gd
+++ src/game.gd
@@ -1,7 +1,52 @@
 extends Control
 
-var card_scene = preload("res://src/card/card.tscn")
+var card_scene := preload("res://src/card/card.tscn")
+var card_played: Node
 
 func _ready():
-	var card = card_scene.instance().init(Color.firebrick, "1")
-	$Grid.add_child(card)
+	card_played = instance_card()
+	$Playpile.add_child(card_played)
+	randomize()
+
+func _on_DrawPile_button_down() -> void:
+	var new_card := instance_card()
+	$Hand.add_child(new_card)
+	update_hand_spacing()
+	
+	# move the card into the correct ordered place
+	var cards = $Hand.get_children()
+	for i in len(cards):
+		if new_card.is_lower_value(cards[i]):
+			$Hand.move_child(new_card, i)
+			break
+
+func _on_Card_button_down(card: Node) -> void:
+	play_card(card)
+
+func play_card(card: Node) -> void:
+	if card.matches_color(card_played) or card.matches_number(card_played):
+		card_played.queue_free()
+		$Hand.remove_child(card)
+		update_hand_spacing()
+		
+		$Playpile.add_child(card)
+		card.rect_position.x = 0
+		card.disconnect("button_down", self, "_on_Card_button_down")
+		card_played = card
+
+func instance_card() -> Node:
+	var color_index := randi() % 4
+	# only supports 0-3 card types right now
+	var type := str(randi() % 4)
+	var card = card_scene.instance().init(type, color_index)
+	card.connect("button_down", self, "_on_Card_button_down", [card])
+	return card
+
+func update_hand_spacing() -> void:
+	var cards = $Hand.get_children()
+	if len(cards) == 0: return
+	
+	var hand_size = $Hand.rect_size.x / 1.5;
+	var space_remaining: float = hand_size - cards[0].rect_size.x * len(cards)
+	var spacing: float = space_remaining / len(cards) - 1
+	$Hand.add_constant_override("separation", min(spacing, -40))
